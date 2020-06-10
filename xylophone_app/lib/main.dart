f9f69c0d65be9ec1bf87c6072d393019f1062a4c
adrian61 <ad.kowalskI@gmail.com>
2020-06-10 23:26:49 +0200

    small fix

--- xylophone_app/lib/main.dart
+++ xylophone_app/lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(XylophoneApp));
+  runApp(MaterialApp(home: XylophoneApp()));
 }
 
 class XylophoneApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-   return Scaffold();
+    return Scaffold();
   }
 }
