bc9fd609012006773878d0238cbf048baa15334c
adrian61 <ad.kowalskI@gmail.com>
2020-06-10 23:46:03 +0200

    Added challenge (created buttons that play music)

--- xylophone_app/lib/main.dart
+++ xylophone_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,6 +9,30 @@ class XylophoneApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            notePlayerButton(Colors.red, 1),
+            notePlayerButton(Colors.orange, 2),
+            notePlayerButton(Colors.yellow, 3),
+            notePlayerButton(Colors.green, 4),
+            notePlayerButton(Colors.greenAccent, 5),
+            notePlayerButton(Colors.blue, 6),
+            notePlayerButton(Colors.purple, 7)
+          ],
+        ),
+      ),
+    );
+  }
+
+  FlatButton notePlayerButton(Color color, int numberOfNote) {
+    return FlatButton(
+        color: color,
+        onPressed: () {
+          AudioCache player = AudioCache();
+          player.play('note$numberOfNote.wav');
+        },
+        child: Text(''));
   }
 }
