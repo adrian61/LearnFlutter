be6ae580407d8a644846cc6e1549bd286e01df5d
adrian61 <ad.kowalskI@gmail.com>
2020-06-12 17:14:35 +0200

    Done step 4

--- destini-challenge-starting-master/lib/story.dart
+++ destini-challenge-starting-master/lib/story.dart
@@ -1,6 +1,11 @@
-class Story{
+class Story {
   String _storyTitle;
   String _choice1;
   String _choice2;
+
+  Story(String story, String choice1, String choice2) {
+    _storyTitle = story;
+    _choice1 = choice1;
+    _choice2 = choice2;
+  }
 }
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
