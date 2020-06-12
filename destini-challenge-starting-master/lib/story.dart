8302d0e6c684f0f7d81a3229762909c72440338a
adrian61 <ad.kowalskI@gmail.com>
2020-06-12 17:18:25 +0200

    Done step 8

--- destini-challenge-starting-master/lib/story.dart
+++ destini-challenge-starting-master/lib/story.dart
@@ -1,10 +1,10 @@
 class Story {
-  String _storyTitle;
+  String storyTitle;
   String _choice1;
   String _choice2;
 
   Story({String storyTitle, String choice1, String choice2}) {
-    _storyTitle = story;
+    storyTitle = storyTitle;
     _choice1 = choice1;
     _choice2 = choice2;
   }
