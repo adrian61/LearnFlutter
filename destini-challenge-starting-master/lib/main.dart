ac8fbd4334791c2e48ccca8e5c7180e3c250b0a4
adrian61 <ad.kowalskI@gmail.com>
2020-06-12 20:44:10 +0200

    Done steps 18-19

--- destini-challenge-starting-master/lib/main.dart
+++ destini-challenge-starting-master/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:destini_challenge_starting/story_brain.dart';
 import 'package:flutter/material.dart';
 
-
 void main() => runApp(Destini());
 
 class Destini extends StatelessWidget {
@@ -13,14 +12,13 @@ class Destini extends StatelessWidget {
   }
 }
 
-
-
 class StoryPage extends StatefulWidget {
   _StoryPageState createState() => _StoryPageState();
 }
 
 class _StoryPageState extends State<StoryPage> {
   StoryBrain storyBrain = StoryBrain();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,58 +31,57 @@ class _StoryPageState extends State<StoryPage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-            Expanded(
-            flex: 12,
-            child: Center(
-              child: Text(
-                storyBrain.getStory(),
-                style: TextStyle(
-                  fontSize: 25.0,
+              Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    storyBrain.getStory(),
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: FlatButton(
-              onPressed: () {
-                //Choice 1 made by user.
-                //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
-              },
-              color: Colors.red,
-              child: Text(
-                storyBrain.getChoice1(),
-                style: TextStyle(
-                  fontSize: 20.0,
+              Expanded(
+                flex: 2,
+                child: FlatButton(
+                  onPressed: () {
+                    //Choice 1 made by user.
+                    storyBrain.nextStory(1);
+                  },
+                  color: Colors.red,
+                  child: Text(
+                    storyBrain.getChoice1(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Expanded(
-            flex: 2,
-            //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
-            //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-            child: FlatButton(
-              onPressed: () {
-                //Choice 2 made by user.
-                //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
-              },
-              color: Colors.blue,
-              child: Text(
-                storyBrain.getChoice2(),
-                style: TextStyle(
-                fontSize: 20.0,
+              SizedBox(
+                height: 20.0,
               ),
-            ),
+              Expanded(
+                flex: 2,
+                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
+                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
+                child: FlatButton(
+                  onPressed: () {
+                    storyBrain.nextStory(2);
+                  },
+                  color: Colors.blue,
+                  child: Text(
+                    storyBrain.getChoice2(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-        ],
       ),
-    ),)
-    ,
     );
   }
 }
