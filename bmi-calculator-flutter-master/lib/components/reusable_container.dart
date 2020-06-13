4f01bfb460cb3dd2bb9dda19fdcf3ae729a2c0c4
adrian61 <ad.kowalskI@gmail.com>
2020-06-13 20:53:57 +0200

    Added slider

--- bmi-calculator-flutter-master/lib/components/reusable_container.dart
+++ bmi-calculator-flutter-master/lib/components/reusable_container.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
 class ReusableContainer extends StatelessWidget {
-  ReusableContainer({@required this.color, this.child});
+  ReusableContainer({@required this.color, this.child, this.onTap});
 
   final Color color;
   final Widget child;
+  final Function onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: child,
-      margin: EdgeInsets.all(15.0),
-      decoration:
-          BoxDecoration(color: color, borderRadius: BorderRadius.circular(10)),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: child,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+            color: color, borderRadius: BorderRadius.circular(10)),
+      ),
     );
   }
 }
--- bmi-calculator-flutter-master/lib/input_page.dart
+++ bmi-calculator-flutter-master/lib/input_page.dart
@@ -4,6 +4,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'components/container_content.dart';
 import 'components/gender_enum.dart';
 import 'components/reusable_container.dart';
+import 'components/constants.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -11,12 +12,11 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-  static const bottomContainerHeight = 80.0;
-  static const inactiveContainerColor = Color(0xFF1D1E33);
-  static const activeContainerColor = Color(0xFF111328);
-  static const bottomContainerColor = Color(0xFFEB1555);
   Color maleCardColor = inactiveContainerColor;
   Color femaleCardColor = inactiveContainerColor;
+  int height = 160;
+  int weight = 60;
+  int age = 20;
 
   @override
   Widget build(BuildContext context) {
@@ -30,30 +30,26 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          updateColor(Gender.male);
-                        });
-                      },
-                      child: ReusableContainer(
-                          color: maleCardColor,
-                          child: ContainerContent(
-                              icon: FontAwesomeIcons.mars, text: 'Male')),
-                    ),
+                    child: ReusableContainer(
+                        onTap: () {
+                          setState(() {
+                            updateColor(Gender.male);
+                          });
+                        },
+                        color: maleCardColor,
+                        child: ContainerContent(
+                            icon: FontAwesomeIcons.mars, text: 'Male')),
                   ),
                   Expanded(
-                    child: GestureDetector(
+                    child: ReusableContainer(
                       onTap: () {
                         setState(() {
                           updateColor(Gender.female);
                         });
                       },
-                      child: ReusableContainer(
-                        color: femaleCardColor,
-                        child: ContainerContent(
-                            icon: FontAwesomeIcons.venus, text: 'Female'),
-                      ),
+                      color: femaleCardColor,
+                      child: ContainerContent(
+                          icon: FontAwesomeIcons.venus, text: 'Female'),
                     ),
                   ),
                 ],
@@ -62,6 +58,29 @@ class _InputPageState extends State<InputPage> {
             Expanded(
               child: ReusableContainer(
                 color: inactiveContainerColor,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'HEIGHT',
+                      style: labelTextStyle,
+                    ),
+                    Text(
+                      height.toString(),
+                      style: numberTextStyle,
+                    ),
+                    Slider(
+                      value: height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                      onChanged: (double value) {
+                        setState(() {
+                          height = value.toInt();
+                        });
+                      },
+                    )
+                  ],
+                ),
               ),
             ),
             Expanded(
@@ -70,11 +89,37 @@ class _InputPageState extends State<InputPage> {
                   Expanded(
                     child: ReusableContainer(
                       color: inactiveContainerColor,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'WEIGHT',
+                            style: labelTextStyle,
+                          ),
+                          Text(
+                            weight.toString(),
+                            style: numberTextStyle,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                   Expanded(
                     child: ReusableContainer(
                       color: inactiveContainerColor,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'AGE',
+                            style: labelTextStyle,
+                          ),
+                          Text(
+                            age.toString(),
+                            style: numberTextStyle,
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ],
